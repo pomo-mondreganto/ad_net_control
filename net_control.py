ea4206b9d4bbbbbaa13dd4f571eee97902ec25ba
Roman Nikitin <nikrom2012@me.com>
2019-11-01 19:09:10 +0300

    Added ban, list & isolation

--- net_control.py
+++ net_control.py
@@ -46,6 +46,19 @@ ALLOW_SSH_RULES = [
 ]
 
 
+def get_isolation_rules(team):
+    return [
+        f'FORWARD -o vuln{team} -j DROP',  # To be inserted after the rule -i teamN -o vulnN
+    ]
+
+
+def get_ban_rules(team):
+    return [
+        f'FORWARD -i team{team} -j DROP',  # To be inserted after the rule -i teamN -o vulnN
+        f'FORWARD -i vuln{team} -j DROP',  # To be inserted after the rule -i vulnN -o teamN
+    ]
+
+
 def run_command(command):
     if not DRY_RUN:
         proc = subprocess.Popen(command)
@@ -63,15 +76,56 @@ def get_team2vuln_rules(team_count):
     )
 
 
+def get_rules_list():
+    command = ['iptables', '-S']
+    out = subprocess.check_output(command)
+    result = out.decode().split('\n')
+    result = list(map(
+        lambda x: ' '.join(x.split(' ')[1:]),
+        filter(lambda x: x, result),
+    ))
+    return result
+
+
+def list_rules(*_args, **_kwargs):
+    rules = get_rules_list()
+    logger.info("Rules:")
+    logger.info('\n'.join(rules))
+
+
+def rule_exists(rule):
+    if DRY_RUN:
+        return False
+
+    command = ['iptables', '-C'] + shlex.split(rule)
+    rc = subprocess.call(command)
+    return rc == 0
+
+
 def add_rules(rules):
     logger.debug('Adding rules:')
     logger.debug('\n'.join(rules))
 
     for rule in rules:
-        command = ['iptables', '-A'] + shlex.split(rule)
-        run_command(command)
+        if not rule_exists(rule):
+            command = ['iptables', '-A'] + shlex.split(rule)
+            run_command(command)
 
     logger.info(f'Done adding {len(rules)} rules')
+
+
+def insert_rules(rules, start):
+    logger.debug("Inserting rules:")
+    logger.debug('\n'.join(rules))
+    logger.debug(f'To a position {start}')
+
+    for i, rule in enumerate(rules):
+        if not rule_exists(rule):
+            index = start + i
+            command = ['iptables', '-I', str(index)] + shlex.split(rule)
+            run_command(command)
+
+    logger.info(f'Done inserting {len(rules)} rules to a position {start}')
 
 
 def remove_rules(rules):
@@ -79,8 +133,9 @@ def remove_rules(rules):
     logger.debug('\n'.join(rules))
 
     for rule in rules:
-        command = ['iptables', '-D'] + shlex.split(rule)
-        run_command(command)
+        if rule_exists(rule):
+            command = ['iptables', '-D'] + shlex.split(rule)
+            run_command(command)
 
     logger.info(f'Done removing {len(rules)} rules')
 
@@ -95,8 +150,12 @@ def remove_drop_rules(*_args, **_kwargs):
     remove_rules(ALLOW_SSH_RULES)
 
 
-def init_network(*, team_count, **_kwargs):
-    rules = INIT_RULES + get_team2vuln_rules(team_count)
+def init_network(*, teams, **_kwargs):
+    if teams is None:
+        logger.error('Specify all required parameters: teams')
+        exit(1)
+
+    rules = INIT_RULES + get_team2vuln_rules(teams)
     add_rules(rules)
     add_drop_rules()
 
@@ -117,10 +176,32 @@ def close_network(*_args, **_kwargs):
     remove_rules(OPEN_NETWORK_RULES)
 
 
-def shutdown_network(*, team_count, **_kwargs):
+def shutdown_network(*, teams, **_kwargs):
+    if teams is None:
+        logger.error('Specify all required parameters: teams')
+        exit(1)
+
     remove_drop_rules()
-    all_rules = OPEN_NETWORK_RULES + INIT_RULES + get_team2vuln_rules(team_count)
+    all_rules = OPEN_NETWORK_RULES + INIT_RULES + get_team2vuln_rules(teams)
     remove_rules(all_rules)
+
+
+def ban_team(teams, team, *_args, **_kwargs):
+    if teams is None or team is None:
+        logger.error('Specify all required parameters: teams, team')
+        exit(1)
+
+    count_before = len(INIT_RULES) + len(get_team2vuln_rules(teams))
+    insert_rules(get_ban_rules(team), count_before)
+
+
+def isolate_team(teams, team, *_args, **_kwargs):
+    if teams is None or team is None:
+        logger.error('Specify all required parameters: teams, team')
+        exit(1)
+
+    count_before = len(INIT_RULES) + len(get_team2vuln_rules(teams))
+    insert_rules(get_isolation_rules(team), count_before)
 
 
 COMMANDS = {
@@ -130,12 +211,16 @@ COMMANDS = {
     'shutdown': shutdown_network,
     'add_drop': add_drop_rules,
     'remove_drop': remove_drop_rules,
+    'list': list_rules,
+    'ban': ban_team,
+    'isolate': isolate_team,
 }
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Manage router network during AD CTF')
     parser.add_argument('command', choices=COMMANDS.keys(), help='Command to run')
-    parser.add_argument('--teams', '-t', dest='team_count', type=int, metavar='N', help='Team count', required=True)
+    parser.add_argument('--teams', '-t', type=int, metavar='N', help='Team count')
+    parser.add_argument('--team', type=int, metavar='N', help='Team number (1-indexed) for ban or isolation')
     parser.add_argument('--verbose', '-v', help='Turn verbose logging on', action='store_true')
     parser.add_argument('--dry-run', help='Just print rules (verbose mode)', action='store_true')
     args = parser.parse_args()
