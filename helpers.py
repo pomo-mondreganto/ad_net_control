ed532572eb39fc78e1affb59c63c681bb203e3f8
Roman Nikitin <nikrom2012@me.com>
2019-11-27 19:48:07 +0300

    Added controller for team-only vpn server

---
+++ helpers.py
@@ -0,0 +1,80 @@
+import logging
+import shlex
+import subprocess
+
+logger = logging.getLogger('ad_net_control')
+logger.addHandler(logging.StreamHandler())
+
+DRY_RUN = False
+
+
+def run_command(command):
+    if not DRY_RUN:
+        proc = subprocess.Popen(command)
+        proc.wait()
+
+
+def get_rules_list():
+    command = ['iptables', '-S']
+    out = subprocess.check_output(command)
+    result = out.decode().split('\n')
+    result = list(map(
+        lambda x: ' '.join(x.split(' ')[1:]),
+        filter(lambda x: x, result),
+    ))
+    return result
+
+
+def list_rules(*_args, **_kwargs):
+    rules = get_rules_list()
+    logger.info("Rules:")
+    logger.info('\n'.join(rules))
+
+
+def rule_exists(rule):
+    if DRY_RUN:
+        return False
+
+    logger.debug(f"Checking if rule {rule} exists")
+
+    command = ['iptables', '-C'] + shlex.split(rule)
+    rc = subprocess.call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return rc == 0
+
+
+def add_rules(rules):
+    logger.debug('Adding rules:')
+    logger.debug('\n'.join(rules))
+
+    for rule in rules:
+        if not rule_exists(rule):
+            command = ['iptables', '-A'] + shlex.split(rule)
+            run_command(command)
+
+    logger.info(f'Done adding {len(rules)} rules')
+
+
+def insert_rules(rules, start):
+    logger.debug("Inserting rules:")
+    logger.debug('\n'.join(rules))
+    logger.debug(f'To a position {start}')
+
+    for i, rule in enumerate(rules):
+        if not rule_exists(rule):
+            index = start + i
+            command = ['iptables', '-I', str(index)] + shlex.split(rule)
+            run_command(command)
+
+    logger.info(f'Done inserting {len(rules)} rules to a position {start}')
+
+
+def remove_rules(rules):
+    logger.debug('Removing rules:')
+    logger.debug('\n'.join(rules))
+
+    for rule in rules:
+        if rule_exists(rule):
+            command = ['iptables', '-D'] + shlex.split(rule)
+            run_command(command)
+
+    logger.info(f'Done removing {len(rules)} rules')
---
+++ team_vpn_server_control.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+
+from helpers import (
+    logger,
+    add_rules,
+    list_rules,
+    insert_rules,
+    remove_rules,
+    DRY_RUN,
+)
+
+INIT_RULES = [
+    'INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT',  # allow already established connections
+    'INPUT -i lo -j ACCEPT',  # accept all local connections
+    'INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT',  # allow icmp 8
+    'INPUT -p icmp --icmp-type 0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT',  # allow icmp 0
+
+    'INPUT -p udp --dport 30001:30999 -j ACCEPT',  # openvpn team servers
+]
+
+DROP_RULES = [
+    'INPUT -j DROP',  # drop all incoming packets that are not explicitly allowed above
+]
+
+ALLOW_SSH_RULES = [
+    'INPUT -p tcp --dport 22 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT',  # ingoing SSH
+    'OUTPUT -p tcp --sport 22 -m state --state RELATED,ESTABLISHED -j ACCEPT',  # outgoing SSH
+]
+
+
+def get_ban_rules(team):
+    return [
+        f'FORWARD -i team{team} -j DROP',
+    ]
+
+
+def add_drop_rules(*_args, **_kwargs):
+    add_rules(ALLOW_SSH_RULES)
+    add_rules(DROP_RULES)
+
+
+def remove_drop_rules(*_args, **_kwargs):
+    remove_rules(DROP_RULES)
+    remove_rules(ALLOW_SSH_RULES)
+
+
+def init_network(**_kwargs):
+    rules = INIT_RULES
+    add_rules(rules)
+    add_drop_rules()
+
+    needs_forwarding = False
+    with open('/proc/sys/net/ipv4/ip_forward', 'r') as f:
+        if f.read().strip() != 1:
+            needs_forwarding = True
+
+    if needs_forwarding:
+        logger.info('Enabling ip forwarding')
+
+        if not DRY_RUN:
+            with open('/proc/sys/net/ipv4/ip_forward', 'w') as f:
+                f.write('1')
+
+
+def shutdown_network(**_kwargs):
+    remove_drop_rules()
+    all_rules = INIT_RULES
+    remove_rules(all_rules)
+
+
+def ban_team(team, *_args, **_kwargs):
+    if team is None:
+        logger.error('Specify all required parameters: team')
+        exit(1)
+
+    insert_rules(get_ban_rules(team), 1)
+
+
+COMMANDS = {
+    'init': init_network,
+    'shutdown': shutdown_network,
+    'add_drop': add_drop_rules,
+    'remove_drop': remove_drop_rules,
+    'list': list_rules,
+    'ban': ban_team,
+}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Manage router network during AD CTF')
+    parser.add_argument('command', choices=COMMANDS.keys(), help='Command to run')
+    parser.add_argument('--verbose', '-v', help='Turn verbose logging on', action='store_true')
+    parser.add_argument('--dry-run', help='Just print rules (verbose mode)', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.verbose or args.dry_run:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    if args.dry_run:
+        DRY_RUN = True
+
+    COMMANDS[args.command](**vars(args))
